dea36be609c0c0eca50e3a3dadb06ae244104eb3
ToanNguyen <toannguyen@SmartDevs-Mac-mini.local>
2022-07-01 11:05:53 +0700

    add todo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
 import "./App.css";
 
+import React, { useState } from "react";
+
 import Nav from "./views/Nav";
 import logo from "./logo.svg";
 
 function App() {
-  const link =
-    "https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=12";
+  const name = "Sao";
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([
+    { id: "todo1", title: "Hoc ReactJs" },
+    { id: "todo2", title: "Hoc Vuejs" },
+  ]);
+
+  const handleAddNewTodo = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const handleSaveTodo = () => {
+    let newTodo = {
+      id: Math.random() * 10,
+      title: todo,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>hello!!!</p>
-        <a href={link} target="_blank">
-          go to my page
-        </a>
+        <p>hello {name}!!!</p>
+        <input
+          type="text"
+          value={todo}
+          onChange={(event) => handleAddNewTodo(event)}
+        />
+        <button onClick={() => handleSaveTodo()}>Save Todo</button>
+        <ul className="todos-container">
+          {todos.map((todo) => {
+            return (
+              <li className="todo-child" key={todo.id}>
+                {todo.title}
+              </li>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
